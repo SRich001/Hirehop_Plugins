d057172b5fc64d4fb379a4cb4039ad0b292909ca
Srich001 <SRich001@outlook.com>
2026-02-03 15:30:05 +0000

    Attempt at inserting net to last grid entry

--- entryTest.js
+++ entryTest.js
@@ -2,12 +2,12 @@ $(document).ready(function()
 	// Adds a Depot Dropdown to the page
 	{
 		//Get all Depots from depots array and put them in the dep object
-		var test = $("<span>",{ html:" Hello"})
 		//Watch for the depot box to change and update the users depot then reload the page
 		
 		// Fill it with depots
 	
-        // Insert the select in place of the depot label
-		$("#accordion").replaceWith(test);
+        // Insert the select in place of the depot labe
+        $("<span>",{ html:" Hello"}).insertAfter($("#search_by_number_content"));
+        
 	});
 
