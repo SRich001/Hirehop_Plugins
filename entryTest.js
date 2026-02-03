38e783cf941776634ec94361f75b034a3d736566
Srich001 <SRich001@outlook.com>
2026-02-03 12:29:22 +0000

    More corrections

--- entryTest.js
+++ entryTest.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function()
 	// Adds a Depot Dropdown to the page
-	
+	{
 		//Get all Depots from depots array and put them in the dep object
-		var test = $("Test")
+		var test = $("<span>",{ html:" Hello"})
 		//Watch for the depot box to change and update the users depot then reload the page
-
-		// Insert the select in place of the depot label
-		$("#search_by_number").replaceWith(test);
+		
+		// Fill it with depots
 	
-    });
-
+        // Insert the select in place of the depot label
+		$("#job_number").replaceWith(test);
+	});
 
