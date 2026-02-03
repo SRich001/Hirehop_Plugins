3b066a0e27f9398bf5387558fc44caf0d645ec1e
Samuel <srich001@outlook.com>
2026-02-03 10:40:49 +0000

    Update demoTwo.js

--- demoTwo.js
+++ demoTwo.js
@@ -1,13 +1,4 @@
-/*!
- * This is a demonstartion of how to interface with HireHop widgets
- * This code will insert the word Hello after the refresh button on
- * the notes widget.
- * 
- * For notes we don't need to wait for the document to be ready using
- * $(document).ready(function(){, as it is loaded before this plugin,
- * but it is necessary to do this for other JavaScript widgets that
- * are loaded afterwards.
-*/
+
 $(document).ready(function(){
 	// Check if the notes widget exists
 	if(typeof($.custom.notes)!='undefined')
@@ -24,4 +15,5 @@ $(document).ready(function(){
 			}
 		});
 	}
+
 });
