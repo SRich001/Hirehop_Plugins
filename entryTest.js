f74c6a139b3b3a14a00fe3b4c584c871b7c91d9d
Srich001 <SRich001@outlook.com>
2026-02-03 12:13:14 +0000

    Correction

--- entryTest.js
+++ entryTest.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 	// Adds a Depot Dropdown to the page
-	{
+	
 		//Get all Depots from depots array and put them in the dep object
 		var test = $("Test")
 		//Watch for the depot box to change and update the users depot then reload the page
 
 		// Insert the select in place of the depot label
 		$("#search_by_number").replaceWith(test);
-	}
+	
+    
+    });
 
 
