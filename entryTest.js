b95d3ceb9f7475a57053cf022634b9b7faa6d158
Srich001 <SRich001@outlook.com>
2026-02-05 10:19:32 +0000

    Variable Test

--- entryTest.js
+++ entryTest.js
@@ -1,13 +1,7 @@
 $(document).ready(function()
-	// Adds a Depot Dropdown to the page
-	{
-		//Get all Depots from depots array and put them in the dep object
-		//Watch for the depot box to change and update the users depot then reload the page
-		
-		// Fill it with depots
 	
-        // Insert the select in place of the depot labe
+	{
         $("<span>",{ html:" Hello"}).insertAfter($("#search_by_number_content"));
-        
+
 	});
 
