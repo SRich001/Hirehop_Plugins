df705989e6ac20b8eb493d89f6ade342a0802c02
Srich001 <SRich001@outlook.com>
2026-02-05 17:21:52 +0000

    minor edit

--- entryTest.js
+++ entryTest.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 	
 	{
-		var stuff = "<p> Whats Up <p/>";
+		var stuff = $("<p> Whats Up <p/>");
 		var curLoc = $(location).prop('href');
 
 		if(curLoc != "https://myhirehop.com/modules/stock/") return;
