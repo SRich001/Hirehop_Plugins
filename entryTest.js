3e505adbfa98fa79cdbb4f0eafdadad2d6a8ef21
Srich001 <SRich001@outlook.com>
2026-02-05 17:07:36 +0000

    minor edit

--- entryTest.js
+++ entryTest.js
@@ -7,6 +7,8 @@ $(document).ready(function()
 
 		if(curLoc != "https://myhirehop.com/modules/stock/") return;
 
+		console.log("did it")
+
         $("<span>",{ html:" Whats Up? "}).insertAfter($("#pq-head-cell-u12-0-5-right"));
 
 
