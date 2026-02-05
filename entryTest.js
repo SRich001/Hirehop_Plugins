b1baa6094309d0e9e6bf2a8b20759da1268deb19
Srich001 <SRich001@outlook.com>
2026-02-05 17:03:51 +0000

    minor edit

--- entryTest.js
+++ entryTest.js
@@ -3,6 +3,8 @@ $(document).ready(function()
 	{
 		var curLoc = $(location).prop('href');
 
+		console.log(curLoc);
+
 		if(curLoc != "https://myhirehop.com/modules/stock/") return;
 
         $("<span>",{ html:" Whats Up? "}).insertAfter($("#pq-head-cell-u12-0-5-right"));
