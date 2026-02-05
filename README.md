49348258ee004e072ec7b1905b8fbe42ddd550c4
Srich001 <SRich001@outlook.com>
2026-02-05 12:46:21 +0000

    file changes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Hirehop Plugins
 Hey, If you're reading this, chances are there's something wrong with one of my plugins.
-Oopsies!
+
+Whoopsies!
--- serial_Search.js
+++ serial_Search.js
@@ -1,3 +1,7 @@
+//PLUGIN: SERIAL_SEARCH  
+//VERSION: 1
+//DESC: Allows you to search for jobs using their serial number on the home page by adding a new input bar that accepts serial numbers.
+
 $(document).ready(function()
 	
 	{
