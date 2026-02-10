efdc981cebe476e2e9a66b7f61346556a34a5db9
Srich001 <SRich001@outlook.com>
2026-02-10 16:29:00 +0000

    Established ajax test

---
+++ ajaxTest.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $ajax.({
+    
+        url: "https://myhirehop.com//modules/consumables/tracked_asset_list.php",
+        method:'get',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (err) {
+            console.long(err);
+        }
+    });
+})
