9cbdc42a2dc91891f25d19e855c2856fda54b50a
Srich001 <SRich001@outlook.com>
2026-02-10 16:42:46 +0000

    minor edit

--- ajaxTest.js
+++ ajaxTest.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 
     $.ajax({
     
-        url: "https:/myhirehop.com//modules/consumables/tracked_asset_list.php",
+        url: "https://myhirehop.com/modules/consumables/tracked_asset_list.php",
         method:'get',
         dataType: 'json',
         success: function (data) {
             console.log(data);
         },
         error: function (err) {
-            console.long(err);
+            console.log(err);
         }
     });
 })
