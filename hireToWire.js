05998219567c777207948435c606d9fc1f905476
Srich001 <SRich001@outlook.com>
2026-02-12 17:17:37 +0000

    minor edit

---
+++ hireToWire.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    if(typeof(user)!=='undefined' && typeof(doc_type)!=='undefined'){
+
+
+        lang.stockManageTxt = "Livewire Stock"
+
+        console.log(lang.stockManageTxt)
+
+    }
+
+})
