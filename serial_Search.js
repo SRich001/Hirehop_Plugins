21d9550da8d76b899e7f3448b355c5374eca0851
Srich001 <SRich001@outlook.com>
2026-02-17 12:10:33 +0000

    minor edit

--- serial_Search.js
+++ serial_Search.js
@@ -1,16 +1,24 @@
-//PLUGIN: SERIAL_SEARCH V13
+//PLUGIN: SERIAL_SEARCH V14
 //DESC: Replaces the barcode search with a Serial Number search and changes instances of "Barcode" to "Serial Number" on all pages. 
 
-function getSerial(){
-          var input = document.getElementById("serial_number").value;
+function getSerial(input){
+          input = document.getElementById("serial_number").value;
           alert(input);
           console.log(input);
           window.location.replace("https://myhirehop.com/modules/stock/#assets_tab");
         };
 
 $(document).ready(function(){
+        
+        var input = "none";
 
-        var searchBar = $(
+        if(curLoc = "https://myhirehop.com/modules/stock/#assets_tab"){
+
+          console.log(input);
+
+        }
+
+        var searchBar = $( 
             "<div style=\"max-width:655px; text-align:right; display:inline-block\">"
               +"<div style=\"margin-top:6px\">"
                 +"<div style=\"width:auto;text-align:right;display:inline-block\" class=\"lang label\" data-label=\"serialNumTxt\">"
@@ -18,7 +26,7 @@ $(document).ready(function(){
                 +"</div>"
                    +"&nbsp;"
                 +"<div style = \"display:inline-block\">"
-                 +"<form action=\"javascript:getSerial();\">"
+                 +"<form action=\"javascript:getSerial(input);\">"
                   +"<input id=\"serial_number\" class=\"numeric_input number_search\" inputMode=\"numeric\" type=\"tel\" maxlength=\"15\">"
                    +"&nbsp;"
                   +"<button class=\"rec_open\" type=\"submit\"></button>"
@@ -30,12 +38,6 @@ $(document).ready(function(){
 
         $("#search_by_number_content").append(searchBar);
 
-
-        //REDIRECT AND PASTE SEARCHED NUMBER AUTOMATICALLY?
-
-        //Extend equipment.js such that it pastes user input in the field. 
-
-        //action=\"https://myhirehop.com/modules/stock/#assets_tab\"
 	});
 
 
