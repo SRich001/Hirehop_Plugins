cdb297ce80a5550087fefa698a3fa23561eca42d
Srich001 <SRich001@outlook.com>
2026-02-17 15:00:54 +0000

    minor edit

--- serial_Search.js
+++ serial_Search.js
@@ -1,4 +1,4 @@
-//PLUGIN: SERIAL_SEARCH V18
+//PLUGIN: SERIAL_SEARCH V23
 //DESC: Replaces the barcode search with a Serial Number search and changes instances of "Barcode" to "Serial Number" on all pages. 
 
 
