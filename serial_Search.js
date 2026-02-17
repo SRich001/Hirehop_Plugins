5c5f204248637c742733aa808b27e929630ad21e
Srich001 <SRich001@outlook.com>
2026-02-17 09:58:38 +0000

    Added redirect to button

--- serial_Search.js
+++ serial_Search.js
@@ -1,8 +1,8 @@
-//PLUGIN: SERIAL_SEARCH V3
+//PLUGIN: SERIAL_SEARCH V4
 //DESC: Replaces the barcode search with a Serial Number search and changes instances of "Barcode" to "Serial Number" on all pages. 
 $(document).ready(function(){
 
-        var stuff = $(
+        var searchBar = $(
             "<div style=\"max-width:655px; text-align:right; display:inline-block\">"
               +"<div style=\"margin-top:6px\">"
                 +"<div style=\"width:auto;text-align:right;display:inline-block\" class=\"lang label\" data-label=\"serialNumTxt\">"
@@ -10,7 +10,7 @@ $(document).ready(function(){
                 +"</div>"
                    +"&nbsp;"
                 +"<div style = \"display:inline-block\">"
-                 +"<form>"
+                 +"<form action=\"https://myhirehop.com/modules/stock/#assets_tab\">"
                   +"<input id=\"serial_number\" class=\"numeric_input number_search\" inputMode=\"numeric\" type=\"tel\" maxlength=\"15\">"
                    +"&nbsp;"
                   +"<button class=\"rec_open\" type=\"submit\"></button>"
@@ -20,7 +20,14 @@ $(document).ready(function(){
             +"</div>")
 
 
-        $("#search_by_number_content").append(stuff);
+        $("#search_by_number_content").append(searchBar);
+
+        //REDIRECT AND PASTE SEARCHED NUMBER AUTOMATICALLY?
+
+        //redirect to #assets_tab
+        //save user input
+        //Extend equipment.js such that it pastes user input in the field. 
+
         
 	});
     
