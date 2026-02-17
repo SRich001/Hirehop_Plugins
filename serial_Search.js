5c8b2b0385a2586a1ca0d9ebbaddec3fb5ffd7f2
Srich001 <SRich001@outlook.com>
2026-02-17 10:24:33 +0000

    minor edit

--- serial_Search.js
+++ serial_Search.js
@@ -1,4 +1,4 @@
-//PLUGIN: SERIAL_SEARCH V4
+//PLUGIN: SERIAL_SEARCH V5
 //DESC: Replaces the barcode search with a Serial Number search and changes instances of "Barcode" to "Serial Number" on all pages. 
 $(document).ready(function(){
 
