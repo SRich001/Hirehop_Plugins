2ee640238d890e35222024db651c7ff77b860f22
Srich001 <SRich001@outlook.com>
2026-02-19 15:47:21 +0000

    minor edit

--- homeBtn.js
+++ homeBtn.js
@@ -1,15 +1,11 @@
-//PLUGIN: HOME_BTN V11
+//PLUGIN: HOME_BTN V12
 //DESC: Adds a dedicated home button to the top of the page. 
 
 function goHome(){
     window.location.replace("https://myhirehop.com/home.php");
 }
 
-$("#home_panel").hover(function() {
-    $(this).addClass("ui-state-hover").removeClass("ui-state-default")
-}, function() {
-    $(this).addClass("ui-state-default").removeClass("ui-state-hover")
-});
+
 
 $(document).ready(function()
 {
@@ -38,8 +34,15 @@ $(document).ready(function()
        + "</div>"
         
     )
-    
+
     $(btn).insertAfter( $( "#help_panel" ) );
+
+
+    $("#home_panel").hover(function() {
+    $(this).addClass("ui-state-hover").removeClass("ui-state-default")
+     }, function() {
+    $(this).addClass("ui-state-default").removeClass("ui-state-hover")
+     });
     
 
 });
