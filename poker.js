02cf5a4034d3b50483b473c41ac5a9cf4f318f7b
Srich001 <SRich001@outlook.com>
2026-02-24 14:32:57 +0000

    minor edit

--- poker.js
+++ poker.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
 
-    var curLoc = $(location).prop('href');
+    // var curLoc = $(location).prop('href');
         
-        if(curLoc != "https://myhirehop.com/home.php") return;
+    //     if(curLoc != "https://myhirehop.com/home.php") return;
 
+    if(typeof($.custom.job_edit)!='undefined')
+        {
 
-          $.widget("custom.job_edit",{
+         $.widget("custom.job_edit",
+            {
 
             _init_main: function()
             {
               this._super(arguments);
 
+              alert("Working!!");
+
               //$("<span>",{ html:" TEST"}).insertAfter(this.memo_section);
-
-
             }
           });
+
+    }
+
+
+         
 
 })
 
