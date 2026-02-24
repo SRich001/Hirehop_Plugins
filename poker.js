1db56c4cf153462444799a13fb2f860b4c9ecb1c
Srich001 <SRich001@outlook.com>
2026-02-24 12:55:44 +0000

    establish edit tester

---
+++ poker.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    var curLoc = $(location).prop('href');
+        
+        if(curLoc != "https://myhirehop.com/home.php") return;
+
+
+          $.widget("custom.job_edit",{
+
+            _init_main: function()
+            {
+              this._super(arguments);
+
+              $("<span>",{ html:" TEST"}).insertAfter(this.memo_section);
+
+            }
+          })
+
+})
+
+
+
+
+    
