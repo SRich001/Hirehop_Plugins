d7976e39d520616395a4bdfae134ef24b93a94e9
Srich001 <SRich001@outlook.com>
2026-02-24 17:11:16 +0000

    minor edit

--- poker.js
+++ poker.js
@@ -1,11 +1,15 @@
-   // var curLoc = $(location).prop('href');
-        
-    //     if(curLoc != "https://myhirehop.com/home.php") return;
-
+   
 
     $(document).ready(function(){
 
+
+        //Add project code bar UI
+        //
+
+        var curLoc = $(location).prop('href');
         
+        if(curLoc != "https://myhirehop.com/home.php") return;
+
 
 
         $.widget("custom.job_edit", $.custom.job_edit,
