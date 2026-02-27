0cea0e424a6abc4cb478542053d4f53090fa06d9
Srich001 <SRich001@outlook.com>
2026-02-27 11:51:36 +0000

    minor edit

--- poker.js
+++ poker.js
@@ -6,6 +6,9 @@
         //Add project code bar UI
         //Lalala
 
+        var curLoc = $(location).prop('href');
+        
+        if(curLoc != "https://myhirehop.com/modules/suppliers/") return;
 
         console.log("Whats up girlie?")
 
