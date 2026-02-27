37fd4aaa91a880a4e0f84a1f8b81be492b2907a8
Srich001 <SRich001@outlook.com>
2026-02-27 15:16:41 +0000

    Established assets button in the bottom pop-up menu in purchase orders.

--- PurchaseToAssets.js
+++ PurchaseToAssets.js
@@ -1,44 +1,30 @@
 $(document).ready(function(){
 
-    //add if statement to check page
 
-    var assetBtnHtml = $(
+function goToAssets2(){
+    window.location.assign("https://myhirehop.com/modules/stock/#assets_tab");
+}
 
-        "<li class=\"menu_edit lockable ui-menu-item\" id=\"ui-id-EX1\" tabindex=\"-1\" role=\"menuitem\">"
-            + "::marker"
-            + "<span class=\"ui-icon ui-icon-pencil\">"
-                + "::after"
-            + "</span>"
-               + "Go to Assets"
+var assetsPop = $(
 
-        + "</li>"
-
-    )
-
-    var assetTabHTML = $(
-
-            "<li data-kind=\"assets\" class=\"assets_tab ui-state-default ui-corner-top\" role=\"tab\" tabindex=\"-1\" aria-controls=\"assets_tab\" aria-labelledby=\"ui-id-5\" aria-selected=\"false\" aria-expanded=\"false\">"
-             + "<a href=\"\" > Assets </a>"
-             +  "</li>"
-
-    )
-
-
-
-    var curLoc = $(location).prop('href');
-
-    if(curLoc != "https://myhirehop.com/modules/suppliers/") return;
-
-    var element = document.querySelectorAll('.ui-button');
-
-    console.log(element);
-
-    
-
+    "<span \"onclick = goToAssets2()\" class=\"ui-icon ui-icon-box\"></span>" 
+    + "Go to Assets"
+)
  
-   
+$.widget("custom.subcontractors", $.custom.subcontractors,
+            {
 
-    //$(assetBtnHtml).insertAfter("#ui-id-24");
-    
+                _init_main: function()
+                {
+
+                    this._super(arguments);
+
+                    $(assetsPop).appendTo(this.bottom_popup_menu); 
+
+                }
+
+            });
+
+
 
 })
