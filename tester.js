6a7d709317359ad56751933e8dca7e60f32cfa88
Srich001 <SRich001@outlook.com>
2026-04-24 15:17:58 +0100

    minor edit

--- tester.js
+++ tester.js
@@ -1,5 +1,43 @@
 
-function goToAssets2()
+
+
+
+$(document).ready(function(){
+
+var curLoc = $(location).prop('href');
+if(curLoc != "https://myhirehop.com/modules/suppliers/") return;
+
+
+
+
+                    
+
+
+//Try pulling from the node data in the go to assets function, in the same way as the subcontractors edit function. 
+//CHECK 4095 SUBCONTRACTOR
+//CHECK REPORTS >> STOCK VALUATION FOR SERIAL NUMBER CALL!!
+
+
+
+$.widget("custom.subcontractors", $.custom.subcontractors,
+            {
+
+                _init_bottom_menu: function()
+                {
+
+                    this._super(arguments);
+
+                    
+                    $( "<li onclick=\"goToAssets2()\" class=\"ui-menu-item\" >"
+                      + "<span class=\"ui-icon ui-icon-box\">"
+                      + "</span>"
+                      + "Go to Assets >>>"
+                      + "</li>" ).appendTo(this.bottom_popup_menu ); 
+ 
+                },
+
+
+                goToAssets2: function()
 {
                        if (this.is_locked()) return;
 
@@ -28,38 +66,6 @@ function goToAssets2()
     
 
                     }   
-                    
-
-
-
-$(document).ready(function(){
-
-var curLoc = $(location).prop('href');
-if(curLoc != "https://myhirehop.com/modules/suppliers/") return;
-
-
-//Try pulling from the node data in the go to assets function, in the same way as the subcontractors edit function. 
-//CHECK 4095 SUBCONTRACTOR
-//CHECK REPORTS >> STOCK VALUATION FOR SERIAL NUMBER CALL!!
-
-
-
-$.widget("custom.subcontractors", $.custom.subcontractors,
-            {
-
-                _init_bottom_menu: function()
-                {
-
-                    this._super(arguments);
-
-                    
-                    $( "<li onclick=\"goToAssets2()\" class=\"ui-menu-item\" >"
-                      + "<span class=\"ui-icon ui-icon-box\">"
-                      + "</span>"
-                      + "Go to Assets >>>"
-                      + "</li>" ).appendTo(this.bottom_popup_menu ); 
- 
-                },
 
 
 
