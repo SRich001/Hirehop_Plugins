706a54e68dac2993acb049b1db3a616a3042b8ea
Srich001 <SRich001@outlook.com>
2026-04-24 13:59:26 +0100

    minor edit

--- tester.js
+++ tester.js
@@ -4,61 +4,60 @@ var curLoc = $(location).prop('href');
 if(curLoc != "https://myhirehop.com/modules/suppliers/") return;
 
 
+
+
 //Try pulling from the node data in the go to assets function, in the same way as the subcontractors edit function. 
 //CHECK 4095 SUBCONTRACTOR
 //CHECK REPORTS >> STOCK VALUATION FOR SERIAL NUMBER CALL!!
 
+
+
 $.widget("custom.subcontractors", $.custom.subcontractors,
             {
 
-                _init_main: function()
+                _init_bottom_menu: function()
                 {
 
                     this._super(arguments);
 
-                    // $( "<li onclick=\"goToAssets2()\" class=\"ui-menu-item\" >"
-                    //   + "<span class=\"ui-icon ui-icon-box\">"
-                    //   + "</span>"
-                    //   + "Go to Assets >>>"
-                    //   + "</li>" ).appendTo(this.bottom_popup_menu ); 
+                    $( "<li onclick=\"goToAssets2()\" class=\"ui-menu-item\" >"
+                      + "<span class=\"ui-icon ui-icon-box\">"
+                      + "</span>"
+                      + "Go to Assets >>>"
+                      + "</li>" ).appendTo(this.bottom_popup_menu ); 
 
-                    this.goToAssets2();
- 
-                },
 
-                goToAssets2: function() {
-                   
 
-                   if (this.is_locked()) return;
+                function goToAssets2(){
+                       if (this.is_locked()) return;
 
-	               var that = this;
+	                   var that = this;
 	
-	               var sub_rowid = that.grid.jqGrid('getGridParam','selrow');
+	                   var sub_rowid = that.grid.jqGrid('getGridParam','selrow');
 
-	               if (!sub_rowid) return;
+	                   if (!sub_rowid) return;
 
-                   var node = false;
+                       var node = false;
 
-                   node = this.supplying.jstree("get_selected", true);
+                       node = this.supplying.jstree("get_selected", true);
 
-	               if (node.length != 1)
-	               {
-		           error_message("You can only edit one item at once.");
-		           return;
-	               }
-	               else
-		           node = node[0];
+	                   if (node.length != 1)
+	                   {
+		               error_message("You can only edit one item at once.");
+		               return;
+	                   }
+	                   else
+		               node = node[0];
 
-                   if(node){
-                      alert(node.data.ITEM);
-                      console.log("data found")
-                    }
-
-                    console.log("end")
+                       if(node){
+                          alert(node.data.ITEM);
+                        }
 
     
 
-                   }
+                    }                  
+ 
+                },
 
                   });
 
