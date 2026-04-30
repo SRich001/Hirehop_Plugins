ea25cf3b6b6c9392e75c4878ca9d3f0304b4db30
Srich001 <SRich001@outlook.com>
2026-04-30 14:05:48 +0100

    minor edit

--- tester.js
+++ tester.js
@@ -49,7 +49,7 @@ $.widget("custom.subcontractors", $.custom.subcontractors,
                     $(assetsJumpBtn).appendTo(this.edit_item_dialog)
                     
                     $("#assetsJumpBtn").click(function(){
-                      item = JSON.stringify(that.edt_desc).slice(1, -1);
+                      item = JSON.stringify(that.edt_text.val()).slice(1, -1);
                       urlItem = item.replaceAll(' ', '_');
                       url = "https://myhirehop.com/modules/stock/?item=" + urlItem + "#assets_tab";
                       alert(urlItem);
